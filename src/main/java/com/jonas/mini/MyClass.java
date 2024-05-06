47a075613e0c881b1d169de5a6d10ab89dde16b0
Jonas Ullvahn <ullvahn@gmail.com>
2024-05-06 14:53:01 +0200

    update test

--- src/main/java/com/jonas/mini/MyClass.java
+++ src/main/java/com/jonas/mini/MyClass.java
@@ -3,7 +3,7 @@ package com.jonas.mini;
 public class MyClass {
 
     public String toNameFormat(String input) {
-        if (!input.matches("[a-öA-Ö\\s]+")) return null;
+        if (!input.matches("[\\p{L}\\s]+")) return null;
         return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
     }
 
